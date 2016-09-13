422a0e133bcf5a19ba310d0c173a0c9dd70d45c0
kaylynyuh <yuhkay4@gmail.com>
2016-09-12 19:36:15 -0700

    greet function working

---
+++ greet.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const greet = require('./index.js');
+
+greet('kale');
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function greet(name) {
+  console.log('hello ' + name );
+};
