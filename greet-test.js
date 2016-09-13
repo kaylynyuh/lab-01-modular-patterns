e7c2d32618f540cd4b4beec675ab59799e8dd135
kaylynyuh <yuhkay4@gmail.com>
2016-09-12 19:45:06 -0700

    test module added

---
+++ greet-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const greet = require('./greet.js');
+const assert = require('assert');
+
+describe('testing greet module', function() {
+  describe('#greet()', function() {
+    it('should say hello plus kale', function() {
+      assert.ok(true === false, 'all good so far');
+      let result = greet.greet('kale');
+      assert.ok(result === 'hello kale', 'was not hello kale');
+    });
+    it('should throw missing name error', function() {
+      assert.throws(function (){
+        greet.greet();
+      }, 'error thrown');
+    });
+  });
+});
